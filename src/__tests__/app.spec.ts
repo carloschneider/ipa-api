25f1526815228e107c56bbfe4c921e846cc7e734
Carlo Schneider <contact@carloschneider.dev>
2022-07-16 19:47:09 -0300

    feat!: separate app and listen; add supertest

---
+++ src/__tests__/app.spec.ts
@@ -0,0 +1,32 @@
+import axios from 'axios'
+import request from 'supertest'
+
+import { app } from '../app'
+import { HTML } from '../scrapings/__mocks__'
+
+jest.mock('axios')
+const mockedAxios = axios as jest.Mocked<typeof axios>
+
+describe('App', () => {
+  it('should return json with word pronunciantion', async () => {
+    mockedAxios.get.mockResolvedValue({
+      data: HTML
+    })
+
+    const response = await request(app)
+      .get('/world')
+      .expect(200)
+
+    expect(response.body.pronunciation).toEqual('wɜrld')
+  })
+
+  it('should return error', async () => {
+    mockedAxios.get.mockRejectedValueOnce(new Error('Mocked error'))
+
+    const response = await request(app)
+      .get('/world')
+      .expect(500)
+
+    expect(response.body.error).toEqual('Mocked error')
+  })
+})
---
+++ src/app.ts
@@ -0,0 +1,40 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+import { CollinsService } from './services/collins'
+import { CollinsScraping } from './scrapings/collins'
+
+const app = express()
+
+app.get('/:word', async (req, res) => {
+  const {
+    params: {
+      word
+    }
+  } = req
+
+  try {
+    const Service = new CollinsService('english', word)
+    const ipaHTML = await Service.getIpa()
+
+    const Scraping = new CollinsScraping(ipaHTML)
+
+    return res.json({
+      pronunciation: Scraping.getPronunciation()
+    })
+
+  } catch (error) {
+    let message = 'Unknown error'
+    if (error instanceof Error) message = error.message
+
+    res.status(500).json({
+      error: message
+    })
+  }
+})
+
+export {
+  app
+}
